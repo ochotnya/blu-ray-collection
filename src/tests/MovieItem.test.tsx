43284350a8fee37a20ffd6782ff2c24dd7942907
adrian <ochotny.a@gmail.com>
2022-03-21 12:48:33 +0100

    Deleted unused imports from MovieItem test

--- src/tests/MovieItem.test.tsx
+++ src/tests/MovieItem.test.tsx
@@ -1,6 +1,5 @@
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
-import { Route, Routes } from "react-router-dom";
 import MovieItem from "../components/MovieItem";
 import { IFirestoreMovie } from "../interfaces/IFirestoreMovie";
 
